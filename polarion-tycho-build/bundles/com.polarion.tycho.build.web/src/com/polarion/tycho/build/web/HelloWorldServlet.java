2fbcdbeb0db89d4c0cab5af97c8dc840b27edab1
Roland Nguyen <roland.nguyen@cip-alpha.eu>
2022-06-08 18:10:09 +0200

    Add sample web bundle with dependency to service bundle

---
+++ polarion-tycho-build/bundles/com.polarion.tycho.build.web/src/com/polarion/tycho/build/web/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+package com.polarion.tycho.build.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.polarion.platform.core.PlatformContext;
+import com.polarion.tycho.build.service.Service;
+
+public class HelloWorldServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -5329632843187790946L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Service svc = PlatformContext.getPlatform().lookupService(Service.class);
+		resp.getWriter().println(svc.doService());
+	}
+}
